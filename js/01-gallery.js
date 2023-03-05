670b182aa44c1575309e5446cb4f78e230955240
Inna-Olkhovskaya <115504107+Inna-Olkhovskaya@users.noreply.github.com>
2023-03-06 00:21:55 +0200

    finished task #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
@@ -39,3 +39,49 @@ basicLightbox.
 Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно.
 У библиотеки basicLightbox есть метод для программного закрытия модального окна.*/
 
+const basicLightbox = window.basicLightbox;
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+}
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+galleryContainer.addEventListener("click", handleGalleryClick);
+
+function handleGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const modalImg = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${modalImg}" width="800" height="600">
+`);
+
+  instance.show();
+
+  window.addEventListener("keydown", onEscKeyPress);
+
+  function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
